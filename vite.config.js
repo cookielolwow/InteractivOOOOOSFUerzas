479e92a46b7e50bf0c9a59d7fd31cd316edfe24a
Mariana Mejia 🪼 <marianamejiagallego@gmail.com>
2026-09-01 22:21:00 -0500

    aaa

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vite';
 
-// Relative assets make the same build work locally and under
-// https://<user>.github.io/<repository>/ without hard-coding the repo name.
 export default defineConfig({
-  base: './',
-  build: {
-    target: 'es2022'
-  }
+  base: '/InteractivOOOOOSFUerzas/',
 });
